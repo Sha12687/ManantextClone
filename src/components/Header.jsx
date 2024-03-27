94d2230d7a1980adc1df0217a7b304e6834dc74a
Shashant <shashantchaudhary999@gmail.com>
2024-03-27 14:35:35 +0530

    fix header comp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,58 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
-    return (
-        <div className=' h-12 flex justify-between items-center bg-slate-500 '>
-            <div className=''>
-                <h1>logo</h1>
-            </div>
-            <div className="fle">
-                <ul>
-                    <li className='text-gray-950'>Home</li>
-                    <li>About</li>
-                    <li>OUR Advantage </li>
-                    <li>Product</li>
-                    <li>sustainability</li>
-                    <li>certificate</li>
-                    <li>contact us</li>
-                </ul>
-            </div>
-        </div>
-    )
+  const [isAboutDropdownOpen, setIsAboutDropdownOpen] = useState(false);
+
+  const [isProductDropdownOpen, setIsProductDropdownOpen] = useState(false);
+  return (
+    <div className="px-5 h-12 flex justify-between items-center bg-white">
+      <div className="">
+        <h1>logo</h1>
+      </div>
+      <div className="flex">
+        <ul className="flex space-x-3">
+          <li className="text-gray-950">Home</li>
+          <li
+            onMouseEnter={() => setIsAboutDropdownOpen(true)}
+            onMouseLeave={() => setIsAboutDropdownOpen(false)}
+            className="relative"
+          >
+            About
+            {isAboutDropdownOpen && (
+              <ul className="absolute top-full left-0 bg-white shadow-md mt-1 py-2 px-3 text-nowrap">
+                <li>Dropdown Item 1</li>
+                <li>Dropdown Item 2</li>
+                <li>Dropdown Item 3</li>
+              </ul>
+            )}
+          </li>
+        
+          <li
+            onMouseEnter={() => setIsProductDropdownOpen(true)}
+            onMouseLeave={() => setIsProductDropdownOpen(false)}
+            className="relative"
+          >
+            Product
+            {isProductDropdownOpen && (
+              <ul className="absolute top-full left-0 bg-white shadow-md mt-1 py-2 px-3">
+                <li>Dropdown Item 1</li>
+                <li>Dropdown Item 2</li>
+                <li>Dropdown Item 3</li>
+              </ul>
+            )}
+          </li>
+          <li>sustainability</li>
+          <li>certificate</li>
+          <li>contact us</li>
+          <li>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
