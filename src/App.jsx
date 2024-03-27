7635d63fc6ffc3c41d5476784d39fa6fabf4f97e
Shashant <shashantchaudhary999@gmail.com>
2024-03-27 14:43:55 +0530

    fix header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import TopHeader from './components/TopHeader'
 import Header from './components/Header'
+import Home from './Pages/Home'
 
 function App() {
   return (
     <>
-      <TopHeader />
-      <Header />
+    <Home/>
     </>
   )
 }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import TopHeader from "../components/TopHeader"
+import Header  from '../components/Header'
+export default function Home() {
+  return (
+    <>
+    <TopHeader></TopHeader>
+    <Header></Header>
+    </>
+  )
+}
